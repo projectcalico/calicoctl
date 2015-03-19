6efeb5a9fefe565efd55718cb286227e22631ea6
Spike Curtis <spike.curtis@metaswitch.com>
2015-03-19 15:08:34 -0700

    Set version v0.2.0

--- calicoctl.py
+++ calicoctl.py
@@ -30,7 +30,7 @@ Options:
  --ip6=<IP6>              The local IPv6 management address to use.
  --node-image=<DOCKER_IMAGE_NAME>    Docker image to use for
                           Calico's per-node container
-                          [default: calico/node:latest]
+                          [default: calico/node:v0.2.0]
 
 """
 import socket
