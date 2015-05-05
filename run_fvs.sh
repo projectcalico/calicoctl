f33d3b70f4e95f20f6aed78434a1ad48c4397b0d
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-05 00:32:20 -0700

    Allow running tests for one or more hosts.

--- run_fvs.sh
+++ run_fvs.sh
@@ -6,16 +6,27 @@ date
 pwd
 git status
 
-./build_node.sh
-pushd ./build_calicoctl
-./create_binary.sh
-popd
+if $HOST; then
+    docker build -t host ./host
 
-# Run the FVs
-sudo ./tests/fv/arg_parsing.sh
-sudo ./tests/fv/mainline.sh
-sudo ./tests/fv/add_container.sh
-sudo ./tests/fv/unix_socket.sh
-sudo ./tests/fv/add_ip.sh
+    # Run the FVs
+    docker exec host1 sudo ./tests/fv/arg_parsing.sh
+    docker exec host1 sudo ./tests/fv/mainline.sh
+    docker exec host1 sudo ./tests/fv/add_container.sh
+    docker exec host1 sudo ./tests/fv/unix_socket.sh
+    docker exec host1 sudo ./tests/fv/add_ip.sh
+else
+    ./build_node.sh
+    pushd ./build_calicoctl
+    ./create_binary.sh
+    popd
+
+    # Run the FVs
+    sudo ./tests/fv/arg_parsing.sh
+    sudo ./tests/fv/mainline.sh
+    sudo ./tests/fv/add_container.sh
+    sudo ./tests/fv/unix_socket.sh
+    sudo ./tests/fv/add_ip.sh
+fi
 
 echo "All tests have passed."
