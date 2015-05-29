7c5c99bd37f5fda13f1a287feb663185fd5909ba
Alex Altair <Alex.Altair@metaswitch.com>
2015-05-29 11:33:14 -0700

    Use semaphoreci cache for docker images.

--- calico_containers/tests/st/__init__.py
+++ calico_containers/tests/st/__init__.py
@@ -7,18 +7,22 @@ def setup_package():
     """
     Sets up docker images and host containers for running the STs.
     """
+    if "SEMAPHORE_CACHE_DIR" in os.environ:
+        tar_dir = os.environ["SEMAPHORE_CACHE_DIR"]
+    else:
+        tar_dir = os.path.abspath('.')
     # Pull and save each image, so we can use them inside the host containers.
     print sh.bash("./build_node.sh").stdout
-    docker.save("--output", "calico-node.tar", "calico/node")
-    if not os.path.isfile("busybox.tar"):
+    docker.save("--output", os.path.join(tar_dir, "calico-node.tar"), "calico/node")
+    if not os.path.isfile(os.path.join(tar_dir, "busybox.tar")):
         docker.pull("busybox:latest")
-        docker.save("--output", "busybox.tar", "busybox:latest")
-    if not os.path.isfile("nsenter.tar"):
+        docker.save("--output", os.path.join(tar_dir, "busybox.tar"), "busybox:latest")
+    if not os.path.isfile(os.path.join(tar_dir, "nsenter.tar")):
         docker.pull("jpetazzo/nsenter:latest")
-        docker.save("--output", "nsenter.tar", "jpetazzo/nsenter:latest")
-    if not os.path.isfile("etcd.tar"):
+        docker.save("--output", os.path.join(tar_dir, "nsenter.tar"), "jpetazzo/nsenter:latest")
+    if not os.path.isfile(os.path.join(tar_dir, "etcd.tar")):
         docker.pull("quay.io/coreos/etcd:v2.0.10")
-        docker.save("--output", "etcd.tar", "quay.io/coreos/etcd:v2.0.10")
+        docker.save("--output", os.path.join(tar_dir, "etcd.tar"), "quay.io/coreos/etcd:v2.0.10")
 
     # Create the calicoctl binary here so it will be in the volume mounted on the hosts.
     print sh.bash("./create_binary.sh")
