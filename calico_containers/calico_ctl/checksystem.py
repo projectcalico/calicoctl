85afdbfcb055af754570ca81d7d545802106ddac
Dan Osborne <daniel.osborne@metaswitch.com>
2015-07-07 14:38:08 -0700

    moved submodule functionality into own files

--- calico_containers/calico_ctl/checksystem.py
+++ calico_containers/calico_ctl/checksystem.py
@@ -7,3 +7,116 @@ Usage:
 Options:
  --fix  Allow calicoctl to attempt to modify the host system to fix any encountered issues
 """
+import sys
+import sh
+import docker
+from utils import DOCKER_VERSION
+from utils import enforce_root
+from utils import sysctl
+from utils import docker_client
+
+
+def checksystem(arguments):
+    check_system(arguments["--fix"], quit_if_error=True)
+
+def check_system(fix=False, quit_if_error=False):
+    """
+    Checks that the system is setup correctly. fix==True, this command will
+    attempt to fix any issues it encounters. If any fixes fail, it will
+    exit(1). Fix will automatically be set to True if the user specifies --fix
+    at the command line.
+
+    :param fix: if True, try to fix any system dependency issues that are
+    detected.
+    :param quit_if_error: if True, quit with error code 1 if any issues are
+    detected, or if any fixes are unsuccesful.
+    :return: True if all system dependencies are in the proper state, False if
+    they are not. This function will sys.exit(1) instead of returning false if
+    quit_if_error == True
+    """
+    # modprobe and sysctl require root privileges.
+    enforce_root()
+
+    system_ok = True
+    modprobe = sh.Command._create('modprobe')
+    ip6tables = sh.Command._create('ip6tables')
+    try:
+        ip6tables("-L")
+    except:
+        if fix:
+            try:
+                modprobe('ip6_tables')
+            except sh.ErrorReturnCode:
+                print >> sys.stderr, "ERROR: Could not enable ip6_tables."
+                system_ok = False
+        else:
+            print >> sys.stderr, "WARNING: Unable to detect the ip6_tables " \
+                                 "module. Load with `modprobe ip6_tables`"
+            system_ok = False
+
+    if not module_loaded("xt_set"):
+        if fix:
+            try:
+                modprobe('xt_set')
+            except sh.ErrorReturnCode:
+                print >> sys.stderr, "ERROR: Could not enable xt_set."
+                system_ok = False
+        else:
+            print >> sys.stderr, "WARNING: Unable to detect the xt_set " \
+                                 "module. Load with `modprobe xt_set`"
+            system_ok = False
+
+    # Enable IP forwarding since all compute hosts are vRouters.
+    # IPv4 forwarding should be enabled already by docker.
+    if "1" not in sysctl("net.ipv4.ip_forward"):
+        if fix:
+            if "1" not in sysctl("-w", "net.ipv4.ip_forward=1"):
+                print >> sys.stderr, "ERROR: Could not enable ipv4 forwarding."
+                system_ok = False
+        else:
+            print >> sys.stderr, "WARNING: ipv4 forwarding is not enabled."
+            system_ok = False
+
+    if "1" not in sysctl("net.ipv6.conf.all.forwarding"):
+        if fix:
+            if "1" not in sysctl("-w", "net.ipv6.conf.all.forwarding=1"):
+                print >> sys.stderr, "ERROR: Could not enable ipv6 forwarding."
+                system_ok = False
+        else:
+            print >> sys.stderr, "WARNING: ipv6 forwarding is not enabled."
+            system_ok = False
+
+    # Check docker version compatability
+    try:
+        info = docker_client.version()
+    except docker.errors.APIError:
+        print >> sys.stderr, "ERROR: Docker server must support Docker " \
+                             "Remote API v%s or greater." % DOCKER_VERSION
+        system_ok = False
+    else:
+        api_version = normalize_version(info['ApiVersion'])
+        # Check that API Version is above the minimum supported version
+        if cmp(api_version, normalize_version(DOCKER_VERSION)) < 0:
+            print >> sys.stderr, "ERROR: Docker server must support Docker " \
+                                 "Remote API v%s or greater." % DOCKER_VERSION
+            system_ok = False
+
+    if quit_if_error and not system_ok:
+        sys.exit(1)
+
+    return system_ok
+
+
+def module_loaded(module):
+    return any(s.startswith(module) for s in open("/proc/modules").readlines())
+
+
+def normalize_version(version):
+    """
+    This function convers a string representation of a version into
+    a list of integer values.
+    e.g.:   "1.5.10" => [1, 5, 10]
+    http://stackoverflow.com/questions/1714027/version-number-comparison
+    """
+    return [int(x) for x in re.sub(r'(\.0+)*$', '', version).split(".")]
+
--- calico_containers/calico_ctl/pool.py
+++ calico_containers/calico_ctl/pool.py
@@ -9,3 +9,83 @@ Options:
  --ipv4                   Show IPv4 information only.
  --ipv6                   Show IPv6 information only.
  """
+import sys
+from pycalico.datastore_datatypes import IPPool
+from utils import get_container_ipv_from_arguments
+from utils import client
+from utils import check_ip_version
+from netaddr import IPNetwork
+from prettytable import PrettyTable
+
+
+def pool(arguments):
+    ip_version = get_container_ipv_from_arguments(arguments)
+    if arguments.get("pool"):
+        if arguments.get("add"):
+            ip_pool_add(arguments.get("<CIDR>"),
+                        ip_version,
+                        arguments.get("--ipip"),
+                        arguments.get("--nat-outgoing"))
+        elif arguments.get("remove"):
+            ip_pool_remove(arguments.get("<CIDR>"), ip_version)
+        elif arguments.get("show"):
+            if not ip_version:
+                ip_pool_show("v4")
+                ip_pool_show("v6")
+            else:
+                ip_pool_show(ip_version)
+
+
+def ip_pool_add(cidr_pool, version, ipip, masquerade):
+    """
+    Add the the given CIDR range to the IP address allocation pool.
+
+    :param cidr_pool: The pool to set in CIDR format, e.g. 192.168.0.0/16
+    :param version: v4 or v6
+    :param ipip: Use IP in IP for this pool.
+    :return: None
+    """
+    if version == "v6" and ipip:
+        print "IP in IP not supported for IPv6 pools"
+        sys.exit(1)
+
+    cidr = check_ip_version(cidr_pool, version, IPNetwork)
+    pool = IPPool(cidr, ipip=ipip, masquerade=masquerade)
+    client.add_ip_pool(version, pool)
+
+
+def ip_pool_remove(cidr_pool, version):
+    """
+    Add the the given CIDR range to the IP address allocation pool.
+
+    :param cidr_pool: The pool to set in CIDR format, e.g. 192.168.0.0/16
+    :param version: v4 or v6
+    :return: None
+    """
+    cidr = check_ip_version(cidr_pool, version, IPNetwork)
+    try:
+        client.remove_ip_pool(version, cidr)
+    except KeyError:
+        print "%s is not a configured pool." % cidr_pool
+
+
+def ip_pool_show(version):
+    """
+    Print a list of IP allocation pools.
+    :return: None
+    """
+    assert version in ("v4", "v6")
+    headings = ["IP%s CIDR" % version, "Options"]
+    pools = client.get_ip_pools(version)
+    x = PrettyTable(headings)
+    for pool in pools:
+        enabled_options = []
+        if version == "v4":
+            if pool.ipip:
+                enabled_options.append("ipip")
+            if pool.masquerade:
+                enabled_options.append("nat-outgoing")
+        # convert option array to string
+        row = [str(pool.cidr), ','.join(enabled_options)]
+        x.add_row(row)
+    print x.get_string(sortby=headings[0])
