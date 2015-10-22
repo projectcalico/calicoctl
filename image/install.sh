5258f7c55d942e402a0e8125fc8b01b12cd1dd86
Casey D <casey.davenport@metaswitch.com>
2015-10-21 17:54:13 -0700

    Update to calico-docker v0.9.0.  Pin libcalico@v0.3.1, calico@v1.1.0

--- image/install.sh
+++ image/install.sh
@@ -17,5 +17,5 @@ apt-get install -qy \
 curl -L https://github.com/projectcalico/confd/releases/download/v0.10.0-scale/confd -o confd
 chmod +x confd
 
-pip install git+https://github.com/projectcalico/calico.git
+pip install git+https://github.com/projectcalico/calico.git@1.1.0
 
