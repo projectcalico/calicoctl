db66c6c0b9124031e14351b7b05a9711b1298bc4
gunjan5 <patelgunjan5@gmail.com>
2017-03-15 16:42:49 -0700

    f

--- tests/st/calicoctl/test_node_run_ifprefix.py
+++ tests/st/calicoctl/test_node_run_ifprefix.py
@@ -15,6 +15,8 @@ from tests.st.test_base import TestBase
 from tests.st.utils.docker_host import DockerHost
 from tests.st.utils.exceptions import CommandExecError
 from tests.st.utils.utils import retry_until_success
+from tests.st.utils.utils import ETCD_CA, ETCD_CERT, \
+    ETCD_KEY, ETCD_HOSTNAME_SSL, ETCD_SCHEME, get_ip
 
 """
 Test calicoctl node run --docker-networking-ifprefix=<IFPREFIX>
@@ -23,12 +25,31 @@ Most of the status output is checked by the BGP tests, so this module just
 contains a simple return code check.
 """
 
+ADDITIONAL_DOCKER_OPTIONS = "--cluster-store=etcd://%s:2379 " % \
+                                get_ip()
+
 class TestNodeRunIfprefix(TestBase):
     def test_node_run_ifprefix(self):
         """
         Test that the status command can be executed.
         """
-        with DockerHost('host', dind=False, start_calico=True) as host:
-            def node_run_ifprefix():
-                host.execute("ifconfig | grep cali")
-            retry_until_success(node_run_ifprefix, retries=10, ex_class=Exception)
+        with DockerHost('host1',
+                        additional_docker_options=ADDITIONAL_DOCKER_OPTIONS,
+                        start_calico=False) as host1:
+
+            # Start the node on host1 using first-found auto-detection
+            # method.
+            host1.start_calico_node(
+                "--docker-networking-ifprefix=intf")
+
+            host1.create_workload("thingy", "busybox", "bridge", None, [])
+
+            # Attempt to start the node on host2 using can-reach auto-detection
+            # method using a bogus DNS name.  This should fail.
+            try:
+                host1.execute(
+                    "ifconfig | grep intf")
+            except CommandExecError:
+                pass
+            else:
+                raise AssertionError("Command expected to fail but did not")
