0ba192ea4adf1203530688a85d91b52675347059
Eric Son <ericsunghunson@gmail.com>
2020-09-01 23:37:55 -0700

    changed location of breaking point for dry-run so that it doesn't even create Calico API client

--- calicoctl/commands/common/resources.go
+++ calicoctl/commands/common/resources.go
@@ -205,6 +205,11 @@ func ExecuteConfigCommand(args map[string]interface{}, action action) CommandRes
 		log.Debugf("Data: %s", string(d))
 	}
 
+	// if -d flag is true, stop here, otherwise proceed
+	if dryRunFlag := args["--dry-run"]; dryRunFlag == true {
+		return CommandResults{}
+	}
+
 	// Load the client config and connect.
 	cf := args["--config"].(string)
 	cclient, err := clientmgr.NewClient(cf)
@@ -214,10 +219,6 @@ func ExecuteConfigCommand(args map[string]interface{}, action action) CommandRes
 	}
 	log.Infof("Client: %v", cclient)
 
-	// if -d flag is true, stop here, otherwise proceed
-	if dryRunFlag := args["--dry-run"]; dryRunFlag == true {
-		return CommandResults{Client: cclient}
-	}
 
 
 	// Initialise the command results with the number of resources and the name of the
